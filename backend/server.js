1593b7c5e6f69199f0f45d9e665f110c14c91edd
Ivor Gradinski <ivorgradinski@gmail.com>
2024-03-19 01:57:09 +0100

    Added dotenv.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = 4000;
-app.get('/', (req, res) => {
-    res.send('Hello MEAN Stack!');
-});
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_URL = `mongodb+srv://admin:${DB_PASSWORD}@cryptowebapp.xbnscvx.mongodb.net/?retryWrites=true&w=majority&appName=CryptoWebApp`;
+
+
+MongoClient.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) {
+        console.error('Greška prilikom povezivanja s MongoDB bazom podataka:', err);
+        return;
+    }
+
+    console.log('Povezano s MongoDB bazom podataka');
+    const db = client.db();
+
+    app.get('/', (req, res) => {
+        res.send('Hello MEAN Stack!');
+    });
+
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
 });
