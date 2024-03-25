1fb39ddeab4980f2a68dde78c37d49a9fd16c81d
Ivor Gradinski <ivorgradinski@gmail.com>
2024-03-25 17:05:31 +0100

    Adde user model.

---
+++ backend/models/UserModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    username: { type: String },
+    email: { type: String, unique: true },
+    password: { type: String },
+    role: { type: String, enum: ["admin", "user"], default: "user" },
+});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("Users", userSchema);
