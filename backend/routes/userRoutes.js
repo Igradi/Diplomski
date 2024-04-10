71361f0f6611a1313b2068e5732448468eb95d8d
Ivor Gradinski <ivorgradinski@gmail.com>
2024-04-10 14:01:49 +0200

    put,delete.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -52,8 +52,54 @@ async function login(req, res) {
         res.status(500).send('Greška na serveru');
     }
 }
+async function updateUser(req, res) {
+    const { id } = req.params;
+    const { username, email, password } = req.body;
+
+    try {
+        let user = await Users.findById(id);
+
+        if (!user) {
+            return res.status(400).json({ msg: 'Korisnik ne postoji' });
+        }
+
+        user.username = username || user.username;
+        user.email = email || user.email;
+        if (password) {
+            user.password = password;
+        }
+
+        await user.save();
+
+        res.json({ msg: 'Korisnik je uspješno ažuriran' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Greška na serveru');
+    }
+}
+
+async function deleteUser(req, res) {
+    const { id } = req.params;
+
+    try {
+        let user = await Users.findById(id);
+
+        if (!user) {
+            return res.status(400).json({ msg: 'Korisnik ne postoji' });
+        }
+
+        await Users.deleteOne({ _id: id });
+
+        res.json({ msg: 'Korisnik je uspješno obrisan' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Greška na serveru');
+    }
+}
 
 router.post('/register', register);
 router.post('/login', login);
+router.put('/update/:id', updateUser);
+router.delete('/delete/:id', deleteUser);
 
 module.exports = router;
