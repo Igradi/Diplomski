dee3017148fdabdbd7e2b2f570cbd3decc93932b
Ivor Gradinski <ivorgradinski@gmail.com>
2024-04-15 02:03:10 +0200

    Added routing.

--- frontend/crypto-social-network/src/app/app.component.ts
+++ frontend/crypto-social-network/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, LoginComponent, RegisterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
