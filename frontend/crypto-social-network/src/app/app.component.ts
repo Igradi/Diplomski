6885ca22c6011dfa3cb2a01a447bc8be3d86c1e6
Ivor Gradinski <ivorgradinski@gmail.com>
2024-04-21 13:14:07 +0200

    app.component.ts update

--- frontend/crypto-social-network/src/app/app.component.ts
+++ frontend/crypto-social-network/src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, LoginComponent, RegisterComponent, HomeComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'crypto-social-network';
-}
+export class AppComponent { }
