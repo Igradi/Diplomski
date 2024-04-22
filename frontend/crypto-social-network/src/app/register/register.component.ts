7d349d8d0a4bdf388349c72e67e35ffb91fe200b
Ivor Gradinski <ivorgradinski@gmail.com>
2024-04-22 16:55:59 +0200

    Register backend connection.

--- frontend/crypto-social-network/src/app/register/register.component.ts
+++ frontend/crypto-social-network/src/app/register/register.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  username: string = '';
+  email: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient, private router: Router) { }
+
+  onSubmit() {
+    const registerObj = {
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+
+    this.http.post('http://localhost:4000/api/users/register', registerObj).subscribe((res: any) => {
+      this.router.navigate(['/login']);
+    });
+  }
 }
