2b8a00e663fb47aa1085d8150091994e295b055d
Ivor Gradinski <ivorgradinski@gmail.com>
2024-05-09 16:20:01 +0200

    Getting all posts.

--- frontend/crypto-social-network/src/app/components/post-list/post-list.component.ts
+++ frontend/crypto-social-network/src/app/components/post-list/post-list.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from '../../models/post.model';
+import { PostService } from '../../services/post-list.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss'
 })
 export class PostListComponent {
+  posts: Post[] = [];
 
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+    this.getAllPosts();
+  }
+
+  getAllPosts(): void {
+    this.postService.getAllPosts().subscribe(
+      (data: Post[]) => {
+        this.posts = data;
+      },
+      (error) => {
+        console.error('Error fetching posts:', error);
+      }
+    );
+  }
 }
