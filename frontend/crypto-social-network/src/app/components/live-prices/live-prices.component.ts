cb80b31a3f091ca6dce2be60a7fb202396f60d95
Ivor Gradinski <ivorgradinski@gmail.com>
2024-05-13 19:52:37 +0200

    Btc info from api.

--- frontend/crypto-social-network/src/app/components/live-prices/live-prices.component.ts
+++ frontend/crypto-social-network/src/app/components/live-prices/live-prices.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { LivePricesService } from '../../services/live-prices.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-live-prices',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './live-prices.component.html',
   styleUrl: './live-prices.component.scss'
 })
 export class LivePricesComponent {
+  bitcoinData: any;
 
+  constructor(private livePricesService: LivePricesService) { }
+
+  ngOnInit(): void {
+    this.livePricesService.getBitcoinData().subscribe(data => {
+      this.bitcoinData = data;
+    });
+  }
 }
--- frontend/crypto-social-network/src/app/services/live-prices.service.ts
+++ frontend/crypto-social-network/src/app/services/live-prices.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -6,5 +7,20 @@ import { Observable } from 'rxjs';
     providedIn: 'root'
 })
 export class LivePricesService {
+    constructor(private http: HttpClient) { }
 
+    getBitcoinData(): Observable<any> {
+        const url = "https://api.livecoinwatch.com/coins/single";
+        const headers = {
+            'content-type': 'application/json',
+            'x-api-key': 'e35856aa-bf0e-48bd-aa72-28922f418e2e'
+        };
+        const body = {
+            currency: "USD",
+            code: "BTC",
+            meta: true
+        };
+
+        return this.http.post(url, body, { headers });
+    }
 }
