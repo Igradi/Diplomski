4f93c57d354b2a9a290fd7f3b7d9e01053c1b5e3
Ivor Gradinski <ivorgradinski@gmail.com>
2024-05-27 13:41:09 +0200

    Added swiper.js.

--- frontend/crypto-social-network/src/app/components/guest-home/guest-home.component.ts
+++ frontend/crypto-social-network/src/app/components/guest-home/guest-home.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { LivePricesComponent } from '../live-prices/live-prices.component';
+import { ListOfCoinsComponent } from '../list-of-coins/list-of-coins.component';
 
 @Component({
   selector: 'app-guest-home',
   standalone: true,
-  imports: [LivePricesComponent],
+  imports: [LivePricesComponent, ListOfCoinsComponent],
   templateUrl: './guest-home.component.html',
   styleUrl: './guest-home.component.scss'
 })
-export class GuestHomeComponent {
-
-}
+export class GuestHomeComponent { }
--- frontend/crypto-social-network/src/app/components/list-of-coins/list-of-coins.component.ts
+++ frontend/crypto-social-network/src/app/components/list-of-coins/list-of-coins.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { CryptocurrencyListService } from '../../services/cryptocurrency-list.service';
+import { Cryptocurrency } from '../../models/cryptocurrency.model';
 
 @Component({
   selector: 'app-list-of-coins',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './list-of-coins.component.html',
-  styleUrl: './list-of-coins.component.scss'
+  styleUrls: ['./list-of-coins.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ListOfCoinsComponent {
+  cryptocurrencies: Cryptocurrency[] = [];
 
+  constructor(private cryptocurrencyListService: CryptocurrencyListService) { }
+
+  ngOnInit(): void {
+    this.cryptocurrencyListService.getAllCryptocurrencies().subscribe(
+      (data: Cryptocurrency[]) => {
+        this.cryptocurrencies = data;
+      },
+      (error) => {
+        console.error('Error fetching cryptocurrencies:', error);
+      }
+    );
+  }
 }
